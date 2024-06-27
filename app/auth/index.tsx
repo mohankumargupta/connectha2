0815a2bdd694b5be26f74e383a8fe587307f6c8d
Mohan Gupta <mohangupta@live.com>
2024-06-27 18:58:58 +1000

    got access and refresh tokens back

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -1,13 +1,50 @@
+import * as AuthSession from 'expo-auth-session';
 import { useLocalSearchParams } from 'expo-router';
+import { useEffect } from 'react';
 import { View, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function auth() {
   const params = useLocalSearchParams<{code: string, state: string}>();
-  
+  const code = params.code;
+  const state = params.state;  
+
+  const HOMEASSISTANT_CLIENTID = "https://mohankumargupta.github.io";
+  const HOMEASSISTANT_REDIRECT_URI = "https://mohankumargupta.github.io/redirect/bigbutton.html";
+
+  useEffect(() => {
+    async function exchange_token() {
+      if (code == null) return;
+
+      const tokenResponse = await AuthSession.exchangeCodeAsync({
+        clientId: HOMEASSISTANT_CLIENTID,
+        redirectUri: HOMEASSISTANT_REDIRECT_URI,
+        code: code
+      },{
+        tokenEndpoint: `${state}/auth/token`,
+      });
+      console.log(tokenResponse);
+    }
+    exchange_token();
+  }, []);
 
   /*
-  fetch('', {
+  if (code !=null) {
+    const access_token_request =new AccessTokenRequest({
+      clientId: HOMEASSISTANT_CLIENTID,
+      redirectUri: HOMEASSISTANT_REDIRECT_URI,
+      code: code
+    });
+    
+  }
+  */
+
+  
+
+
+
+  /*
+  fetch(`${state}`, {
     method: 'POST',
     headers:{
       'Content-Type': 'application/x-www-form-urlencoded'
@@ -20,8 +57,9 @@ export default function auth() {
   });
   */
 
-  console.log(params.code);
-  console.log(params.state);
+
+
+
 
   return (
     <SafeAreaView>
