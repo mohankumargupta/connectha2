e3336914e36d06367c12e11b1a2265c64e7f385b
Mohan Gupta <mohangupta@live.com>
2024-07-01 12:53:09 +1000

    icon list autocompleting

--- app/home/iconsList.tsx
+++ app/home/iconsList.tsx
@@ -3,15 +3,21 @@ import { View, Text } from 'react-native';
 import { PaperProvider, TextInput } from 'react-native-paper';
 
 // @ts-ignore
-import MaterialCommunityIcons from 'react-native-vector-icons/glyphmaps/MaterialCommunityIcons.json';
+import MDIGlyphMap from 'react-native-vector-icons/glyphmaps/MaterialCommunityIcons.json';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+
+type MaterialIconName = React.ComponentProps<typeof MaterialCommunityIcons>['name'];
+
 
 const icons: { [key in string]: number } = {
-  ...MaterialCommunityIcons,
+  ...MDIGlyphMap,
 };
 
 export default function IconsList() {
+  //const iconName: MaterialIconName =  'play'
+  
   const [query, setQuery] = useState('');
-
+  
   const iconNames = Object.keys(icons).filter(
     (item) =>
       item.includes(query.toLowerCase().replace(/\s/g, '-')) ||
@@ -30,6 +36,9 @@ export default function IconsList() {
        <Text>{iconNames.length}</Text>
        <Text>{iconNames[0]}</Text>
        <Text>{iconNamesExact? "match": "no match"}</Text>
+       <Text style={{fontFamily: "material-community"}}>{String.fromCodePoint(983705)}</Text>
+       
+       { iconNamesExact && <MaterialCommunityIcons name={query} size={24} color="black" /> }
     </PaperProvider>
     
   );
