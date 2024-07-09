5669679ac9e1871b782b77c4cd43ccd284ce201f
Mohan Gupta <mohangupta@live.com>
2024-07-09 17:37:22 +1000

    loading page redirects

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import { useNavigation, useGlobalSearchParams, Redirect, router } from 'expo-router';
-import 'react-native-reanimated';
+//import 'react-native-reanimated';
+import * as SecureStore from 'expo-secure-store';
+import { useEffect, useState } from 'react';
 
 //WebBrowser.maybeCompleteAuthSession();
 
@@ -10,10 +12,38 @@ type HomeAssistantURL = {
   url: string
 };
 
+
 export default function index() {
 
-  return (
-    <Redirect href="/login"></Redirect>
-  );
+  const [access_token, setAccessToken] = useState<string|undefined>(undefined);
+  const [validtoken, setValidToken] = useState<boolean|undefined>(undefined);
+
+  async function get_value_from_store(key: string) {
+    let result = await SecureStore.getItemAsync(key);
+    console.log(result);
+    if (result) {
+      setValidToken(true);
+    }
+  }
+  
+  useEffect(() => {
+    // Call the function when the component mounts
+    get_value_from_store('accessToken');
+  }, []);
+
+  if (validtoken === undefined) {
+    return <></>
+  }
+  else if (validtoken === true) {
+    return (
+      <Redirect href="/home/settings"></Redirect>
+    );
+  }
+
+  else {
+    return (
+      <Redirect href="/login"></Redirect>
+    );
+  }
 }
 
