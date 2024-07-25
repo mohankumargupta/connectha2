4c6b241a296ac594a7066a7095d19d0c1885a5f2
Mohan Gupta <mohangupta@live.com>
2024-07-25 18:24:57 +1000

    changed splash screen

---
+++ assets/images/gradient.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+def generate_gradient(
+        colour1: str, colour2: str, width: int, height: int) -> Image:
+    """Generate a vertical gradient."""
+    base = Image.new('RGB', (width, height), colour1)
+    top = Image.new('RGB', (width, height), colour2)
+    mask = Image.new('L', (width, height))
+    mask_data = []
+    for y in range(height):
+        mask_data.extend([int(255 * (y / height))] * width)
+    mask.putdata(mask_data)
+    base.paste(top, (0, 0), mask)
+    return base
+
+
+
+
+image = generate_gradient('#007260', '#39868D', 1284, 2778)
+image.save('gradient.png')
+
