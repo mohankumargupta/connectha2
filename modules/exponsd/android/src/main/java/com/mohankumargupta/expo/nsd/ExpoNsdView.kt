c92e016ea3361418b7aa94dfa2e961edb25e2ac8
Mohan Gupta <mohangupta@live.com>
2024-08-01 20:11:12 +1000

    trying to troubleshoot native module

---
+++ modules/exponsd/android/src/main/java/com/mohankumargupta/expo/nsd/ExpoNsdView.kt
@@ -0,0 +1,7 @@
+package com.mohankumargupta.expo.nsd
+
+import android.content.Context
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
+
+class ExpoNsdView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
